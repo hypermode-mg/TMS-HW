1b488ee5f11bcab8ebe02f4803f926e3e239d469
Hyper Mode <hypermode@mail.ru>
2026-02-15 15:36:27 +0500

    Add comments for find_files script

--- HW27/find_files.py
+++ HW27/find_files.py
@@ -1,4 +1,5 @@
-# Скрипт, который принимает на вход список чисел и находит медиану этих чисел
+# Скрипт, который принимает на вход список файлов и находит файлы, имена которых
+# содержат определённую подстроку
 
 # Функция поиска файлов, имена которых содержат заданную подстроку.
 def find_files(file_list, condition):
@@ -8,9 +9,12 @@ def find_files(file_list, condition):
             finding_files.append(file_name)
     return finding_files
 
-# Пример использования
+# Взял часть файлов из домашней директории пользователя в виде списка
 files = ['conf','gtk-4.0','ubuntu-xdg-terminals.list','evolution','update-notifier','gnome-initial-setup-done','user-dirs.dirs','gnome-session','nautilus','user-dirs.locale','GNOME-xdg-terminals.list','pulse','xdg-terminals.list','gtk-3.0','tiling-assistant']
+#  Запрашиваем условие для поиска
 condition = input("Введите условие для поиска в именах файлов: ").strip()
+# Вызываем функцию и передаём ей список файлов и условие
 result = find_files(files, condition)
+# Выводим результат
 print("Ваше условие найдено в именах следующих файлов: ")
 print(result)
